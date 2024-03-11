08f56590711455b2013204f62deb1b7a8bc31958
浜田　俊祐 <hamashun0820@icloud.com>
2024-03-11 10:01:30 +0900

    change: ルールを変更

--- preprocess/preprocess.go
+++ preprocess/preprocess.go
@@ -25,36 +25,6 @@ type Word struct {
 	IsLast bool     `json:"is_last"`
 }
 
-type Vowel string
-
-const (
-	VowelA Vowel = "ア"
-	VowelI Vowel = "イ"
-	VowelU Vowel = "ウ"
-	VowelE Vowel = "エ"
-	VowelO Vowel = "オ"
-)
-
-var vowelMap = map[string]Vowel{}
-
-func init() {
-	for _, v := range []string{"ア", "カ", "サ", "タ", "ナ", "ハ", "マ", "ヤ", "ラ", "ワ", "ガ", "ザ", "ダ", "バ", "パ", "ァ", "ャ"} {
-		vowelMap[v] = VowelA
-	}
-	for _, v := range []string{"イ", "キ", "シ", "チ", "ニ", "ヒ", "ミ", "リ", "ギ", "ジ", "ヂ", "ビ", "ピ", "ィ"} {
-		vowelMap[v] = VowelI
-	}
-	for _, v := range []string{"ウ", "ク", "ス", "ツ", "ヌ", "フ", "ム", "ユ", "ル", "グ", "ズ", "ヅ", "ブ", "プ", "ゥ", "ュ", "ッ"} {
-		vowelMap[v] = VowelU
-	}
-	for _, v := range []string{"エ", "ケ", "セ", "テ", "ネ", "ヘ", "メ", "レ", "ゲ", "ゼ", "デ", "ベ", "ペ", "ェ"} {
-		vowelMap[v] = VowelE
-	}
-	for _, v := range []string{"オ", "コ", "ソ", "ト", "ノ", "ホ", "モ", "ヨ", "ロ", "ヲ", "ゴ", "ゾ", "ド", "ボ", "ポ", "ォ", "ョ"} {
-		vowelMap[v] = VowelO
-	}
-}
-
 type SmallKana string
 
 const (
@@ -81,68 +51,6 @@ func init() {
 	largeKanaMap[SmallE] = "エ"
 	largeKanaMap[SmallO] = "オ"
 	largeKanaMap[SmallTsu] = "ツ"
-}
-
-type VoicedSoundKana string
-
-const (
-	VoicedSoundGa VoicedSoundKana = "ガ"
-	VoicedSoundGi VoicedSoundKana = "ギ"
-	VoicedSoundGu VoicedSoundKana = "グ"
-	VoicedSoundGe VoicedSoundKana = "ゲ"
-	VoicedSoundGo VoicedSoundKana = "ゴ"
-	VoicedSoundZa VoicedSoundKana = "ザ"
-	VoicedSoundJi VoicedSoundKana = "ジ"
-	VoicedSoundZu VoicedSoundKana = "ズ"
-	VoicedSoundZe VoicedSoundKana = "ゼ"
-	VoicedSoundZo VoicedSoundKana = "ゾ"
-	VoicedSoundDa VoicedSoundKana = "ダ"
-	VoicedSoundDi VoicedSoundKana = "ヂ"
-	VoicedSoundDu VoicedSoundKana = "ヅ"
-	VoicedSoundDe VoicedSoundKana = "デ"
-	VoicedSoundDo VoicedSoundKana = "ド"
-	VoicedSoundBa VoicedSoundKana = "バ"
-	VoicedSoundBi VoicedSoundKana = "ビ"
-	VoicedSoundBu VoicedSoundKana = "ブ"
-	VoicedSoundBe VoicedSoundKana = "ベ"
-	VoicedSoundBo VoicedSoundKana = "ボ"
-	VoicedSoundPa VoicedSoundKana = "パ"
-	VoicedSoundPi VoicedSoundKana = "ピ"
-	VoicedSoundPu VoicedSoundKana = "プ"
-	VoicedSoundPe VoicedSoundKana = "ペ"
-	VoicedSoundPo VoicedSoundKana = "ポ"
-	VoicedSoundVu VoicedSoundKana = "ヴ"
-)
-
-var voicedSoundMap = map[VoicedSoundKana]string{}
-
-func init() {
-	voicedSoundMap[VoicedSoundGa] = "カ"
-	voicedSoundMap[VoicedSoundGi] = "キ"
-	voicedSoundMap[VoicedSoundGu] = "ク"
-	voicedSoundMap[VoicedSoundGe] = "ケ"
-	voicedSoundMap[VoicedSoundGo] = "コ"
-	voicedSoundMap[VoicedSoundZa] = "サ"
-	voicedSoundMap[VoicedSoundJi] = "シ"
-	voicedSoundMap[VoicedSoundZu] = "ス"
-	voicedSoundMap[VoicedSoundZe] = "セ"
-	voicedSoundMap[VoicedSoundZo] = "ソ"
-	voicedSoundMap[VoicedSoundDa] = "タ"
-	voicedSoundMap[VoicedSoundDi] = "チ"
-	voicedSoundMap[VoicedSoundDu] = "ツ"
-	voicedSoundMap[VoicedSoundDe] = "テ"
-	voicedSoundMap[VoicedSoundDo] = "ト"
-	voicedSoundMap[VoicedSoundBa] = "ハ"
-	voicedSoundMap[VoicedSoundBi] = "ヒ"
-	voicedSoundMap[VoicedSoundBu] = "フ"
-	voicedSoundMap[VoicedSoundBe] = "ヘ"
-	voicedSoundMap[VoicedSoundBo] = "ホ"
-	voicedSoundMap[VoicedSoundPa] = "ハ"
-	voicedSoundMap[VoicedSoundPi] = "ヒ"
-	voicedSoundMap[VoicedSoundPu] = "フ"
-	voicedSoundMap[VoicedSoundPe] = "ヘ"
-	voicedSoundMap[VoicedSoundPo] = "ホ"
-	voicedSoundMap[VoicedSoundVu] = "ウ"
 }
 
 func main() {
@@ -186,36 +94,18 @@ func main() {
 			panic(err)
 		}
 
-		// ルールは(https://w.atwiki.jp/ultimate/pages/16.html)に準拠
-		// 伸ばし棒･小文字･濁音･半濁音を再帰的に処理していく
-		for i := endIndex; i > 0; i-- {
-			back := false
-			// 伸ばし棒で終わる場合
-			if string(end) == "ー" {
-				endIndex--
-				end = r[endIndex : endIndex+1]
-				w.End = append(w.End, string(vowelMap[string(end)])) // 伸ばし棒の直前の文字の母音を追加
-				back = true
-			}
-
-			// 小文字で終わる場合
-			if l, ok := largeKanaMap[SmallKana(end)]; ok {
-				w.End = append(w.End, string(l)) //大文字に変換して追加
-
-				endIndex--
-				end = r[endIndex : endIndex+1]
-				back = true
-			}
-
-			// 濁音･半濁音で終わる場合
-			if l, ok := voicedSoundMap[VoicedSoundKana(end)]; ok {
-				w.End = append(w.End, string(l)) // もとの文字に変換して追加
-			}
-
-			if !back {
-				break
-			}
+		// ルールは(https://pokemon-shiritori.com/howto)に準拠
+		// 伸ばし棒で終わる場合
+		if string(end) == "ー" {
+			endIndex--
+			end = r[endIndex : endIndex+1]
 		}
+
+		// 小文字で終わる場合
+		if l, ok := largeKanaMap[SmallKana(end)]; ok {
+			end = []rune(l)
+		}
+
 		w.End = append(w.End, string(end))
 
 		w.End = slices.Compact(w.End)
@@ -251,4 +141,7 @@ func main() {
 	defer f.Close()
 
 	_, err = f.Write(j)
+	if err != nil {
+		panic(err)
+	}
 }
